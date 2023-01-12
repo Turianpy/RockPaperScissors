cb74af93c37dbe4537c581e1c511932f9bed9f99
Andrey Danilov <kekmaster69@yandex.ru>
2023-01-12 04:02:56 +0200

    adds ui, game data display and custom gameover event

---
+++ rps/rps.js
@@ -0,0 +1,94 @@
+const CHOICE = ['rock', 'paper', 'scissors']
+const pScore = document.getElementById('p-score')
+const cScore = document.getElementById('c-score')
+const pChoice = document.getElementById('player-choice')
+const cChoice = document.getElementById('computer-choice')
+const winner = document.getElementById('winner')
+const rounds = document.getElementById('rounds')
+const images = document.querySelectorAll('img')
+
+let roundsPlayed = 1;
+
+let score = {
+    'p-score': 0,
+    'c-score': 0
+}
+
+function reset() {
+    score = {
+        'p-score': 0,
+        'c-score': 0
+    }
+    roundsPlayed = 0;
+    pScore.textContent = cScore.textContent = ''
+}
+
+function getComputerChoice() {
+    const index = Math.floor(Math.random() * CHOICE.length);
+    cChoice.textContent = `Computer chose ${CHOICE[index]}`
+    return CHOICE[index];
+}
+
+function rps_round(playerSelection, computerSelection, curScore) {
+
+    rounds.textContent = `Round ${roundsPlayed}`
+
+    if (playerSelection== computerSelection) {
+        winner.textContent = "it's a tie"
+    }
+    else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+             (playerSelection === 'paper' && computerSelection === 'rock') ||
+             (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        
+        winner.textContent = "You win!"
+        curScore['p-score']++
+    }
+    else {
+        winner.textContent = "Computer wins!"
+        curScore['c-score']++
+    }
+    pScore.textContent = `Player: ${curScore['p-score']}`
+    cScore.textContent = `Computer: ${curScore['c-score']}`
+    score = curScore
+    roundsPlayed ++
+
+    if ((score["c-score"] == 5) || (score["p-score"] == 5)) {
+        if (score['c-score'] > score['p-score']) {
+            var gameover = new CustomEvent('gameover', {detail: 'computer'})
+        } else {
+            var gameover = new CustomEvent('gameover', {detail: 'player'})
+        }
+        reset()
+        winner.dispatchEvent(gameover)
+    }
+
+}
+
+
+const rockBtn = document.querySelector('#rock img')
+const paperBtn = document.querySelector('#paper img')
+const scissorsBtn = document.querySelector('#scissors img')
+
+rockBtn.addEventListener('click', () => {
+    rps_round('rock', getComputerChoice(), score)
+    pChoice.textContent = 'You chose rock'
+})
+
+paperBtn.addEventListener('click', () => {
+    rps_round('paper', getComputerChoice(), score)
+    pChoice.textContent = 'You chose paper'
+})
+
+scissorsBtn.addEventListener('click', () => {
+    rps_round('scissors', getComputerChoice(), score)
+    pChoice.textContent = 'You chose scissors'
+})
+
+winner.addEventListener('gameover', function handler(e) {
+    if (e.detail == 'player') {
+        alert('player wins!')
+    } else {
+        console.log(e.detail)
+        alert('computer wins!')
+    }
+})
